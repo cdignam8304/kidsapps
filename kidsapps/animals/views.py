0a1ee60729671b52f4ad16f490b3656e38f50c19
Christopher Dignam <dignachr@gmail.com>
2021-10-13 00:30:12 +0200

    Dynamic page displaying content

--- kidsapps/animals/views.py
+++ kidsapps/animals/views.py
@@ -1,7 +1,14 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.views.generic import ListView
+from animals.models import Animal
 
 # Create your views here.
 
-def index(request):
-	return HttpResponse("Hello, world. You're at the animals index.")
+# def index(request):
+# 	return HttpResponse("Hello, world. You're at the animals index.")
+
+class AnimalListView(ListView):
+    model = Animal
+
+
